d5318f2e59d495dbf56b3a636d8a15fc748751c8
Zeqiang Lai <916323301@qq.com>
2023-04-11 16:11:01 +0800

    add copyright

--- classification/main_accelerate.py
+++ classification/main_accelerate.py
@@ -1,3 +1,9 @@
+# --------------------------------------------------------
+# InternImage
+# Copyright (c) 2022 OpenGVLab
+# Licensed under The MIT License [see LICENSE for details]
+# --------------------------------------------------------
+
 import datetime
 import argparse
 import os
