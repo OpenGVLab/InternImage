dc82126fb28ef49cd75908a91f266d0a534347c6
Zeqiang Lai <916323301@qq.com>
2023-04-17 21:42:24 +0800

    add backward compatibility

--- classification/ops_dcnv3/functions/dcnv3_func.py
+++ classification/ops_dcnv3/functions/dcnv3_func.py
@@ -37,11 +37,16 @@ class DCNv3Function(Function):
         ctx.offset_scale = offset_scale
         ctx.im2col_step = im2col_step
         ctx.remove_center = remove_center
+
+        kwargs = {}
+        if remove_center:
+            kwargs['remove_center'] = remove_center
+
         output = DCNv3.dcnv3_forward(
             input, offset, mask, kernel_h,
             kernel_w, stride_h, stride_w, pad_h,
             pad_w, dilation_h, dilation_w, group,
-            group_channels, offset_scale, ctx.im2col_step, remove_center)
+            group_channels, offset_scale, ctx.im2col_step, **kwargs)
         ctx.save_for_backward(input, offset, mask)
 
         return output
@@ -51,12 +56,17 @@ class DCNv3Function(Function):
     @custom_bwd
     def backward(ctx, grad_output):
         input, offset, mask = ctx.saved_tensors
+
+        kwargs = {}
+        if ctx.remove_center:
+            kwargs['remove_center'] = ctx.remove_center
+
         grad_input, grad_offset, grad_mask = \
             DCNv3.dcnv3_backward(
                 input, offset, mask, ctx.kernel_h,
                 ctx.kernel_w, ctx.stride_h, ctx.stride_w, ctx.pad_h,
                 ctx.pad_w, ctx.dilation_h, ctx.dilation_w, ctx.group,
-                ctx.group_channels, ctx.offset_scale, grad_output.contiguous(), ctx.im2col_step, ctx.remove_center)
+                ctx.group_channels, ctx.offset_scale, grad_output.contiguous(), ctx.im2col_step, **kwargs)
 
         return grad_input, grad_offset, grad_mask, \
             None, None, None, None, None, None, None, None, None, None, None, None, None
