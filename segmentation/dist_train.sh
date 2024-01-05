50331b52847ae486784e01970a47b222e6f86495
Cheruscan Arminius <155772048+CheruscanArminius@users.noreply.github.com>
2024-01-05 21:16:16 +0100

    Update dist_train.sh
    Adding multi-node to the training process

--- segmentation/dist_train.sh
+++ segmentation/dist_train.sh
@@ -2,8 +2,9 @@
 
 CONFIG=$1
 GPUS=$2
+NODES=$3
 PORT=${PORT:-29300}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --nnodes=$NODES --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
